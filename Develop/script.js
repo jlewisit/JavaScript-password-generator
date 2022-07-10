28cfce318a8093b75457d1f41ee2a0e4ea3b6edb
jlewisit <joelewis.mortgage@gmail.com>
2022-07-10 06:14:34 -0700

    all var if statements logging digits

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,11 @@
-const lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-const upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-const numbers = ["0","1","2","3","4","5","6","7","8","9"];
-const symbols = ["!","@","#","$","%","^","&","*","(",")","-","=","+"];
+// var lower = ["abcdefghijklmnopqrstuvwxyz"]
+var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var symbols = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+"];
+// var char = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()-_=+ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var char = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+// var Y = true;
 var password = ""
 var generateBtn = document.querySelector("#generate");
 
@@ -14,89 +18,62 @@ function writePassword() {
 
 }
 
-var char = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()-_=+ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const Y = true;
-
-// Generate password characters
 var passwordLength = Number(prompt("Enter a password length between 8 and 128"));
 console.log(passwordLength)
 
+// Selects random lower array element
 var inclLower = prompt("Include lowercase letters? Enter Y or N", "Y");
-if (inclLower === true) { 
+if (inclLower === "Y") { 
   for (var i = 0; i <= lower.length; i++);
-  Math.floor(Math.random() * lower.length);
-  console.log(inclLower)
+  console.log(Math.floor(Math.random() * lower.length));
 } 
 else { 
-  null;// Code moving to next operation
+  console.log("lowercase not included");
 }
 
+// Selects random upper array element
 var inclUpper = prompt("Include uppercase letters? Enter Y or N", "Y");
-if (inclUpper === true) { 
-  Math.floor(Math.random() * upper.length);
-  console.log(inclUpper)
+if (inclUpper === "Y") { 
+  for (var i = 0; i <= upper.length; i++);
+  console.log(Math.floor(Math.random() * upper.length));
 } 
 else { 
-  null;// Code moving to next operation
+  console.log("uppercase not included");
 }
 
+// Selects random numbers array element
 var inclNumbers = prompt("Include numbers? Enter Y or N", "Y");
-if (inclNumbers === true) { 
-  Math.floor(Math.random() * numbers.length);
-  console.log(inclNumbers)
-} 
-else { 
-  null;// Code moving to next operation
-}
+    if (inclNumbers === "Y") {
+    console.log(Math.floor(Math.random() * numbers.length));
+  }
+    else { 
+    console.log("numbers not included");
+  }
 
+// Selects random symbols array element  
 var inclSymbols = prompt("Include symbols?  Enter Y or N", "Y");
-if (inclSymbols === true) {
-  Math.floor(Math.random() * symbols.length);
-  console.log(inclSymbols)
+if (inclSymbols === "Y") {
+  console.log(Math.floor(Math.random() * symbols.length));
 }
 else { 
-  null;// Code moving to next operation
+  console.log("symbols not included");
 }
 
+// This does not work with my arrays
 function generatePassword() {
-  for (let i = 0; i < passwordLength.length; i++) {
-  var randomNum = Math.floor(Math.random * char.length)
-  password += char.substring(randomNum, randomNum +1)
+  for (var i = 0; i <= passwordLength.length; i++) {
+  var randomNum = Math.floor(Math.random * char.length);
+  password += char.substring(randomNum, randomNum +1);
   console.log(password);
   }
 }
 
-
-// (**Starter Code**) Add event listener to generate button
+//(**Starter Code**) Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
 
 
-
-
-
-
-// //Console debugging code
-// function genPopup1() {
-//   var popup = window.open("", "DNH", "width=300,height=100");
-
-//   if (popup) {
-//     popup.document.body.innerHTML = "<p>" + popup.opener.dnhUrl + "</p>";
-    
-//     popup.document.body.innerHTML = popup.document.body.innerHTML + 
-//     "<button onclick='parent.opener.ShowConfirm();'>Trigger Confirm</button>";
-    
-//     popup.document.title = popup.name;
-
-//     popup.opener.document.body.innerHTML = "<p>Loaded!</p>";
-//   } else {
-//     document.body.innerHTML = "<p>Not loading!</p>";
-//   }
-// }
-// function ShowConfirm(){
-// confirm("this window is active?");
-// }
 
 
 
