ab78bddc58fa2ad399f97886e0d0b2a34295b6d3
jlewisit <joelewis.mortgage@gmail.com>
2022-07-10 17:23:55 -0700

    js and html updates

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,6 @@ const lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","
 const upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const numbers = ["0","1","2","3","4","5","6","7","8","9"];
 const symbols = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+"];
-
 // Object containing all potential characters
 var characters = {
   lower: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
@@ -10,78 +9,104 @@ var characters = {
   numbers: ["0","1","2","3","4","5","6","7","8","9"],
   symbols: ["!","@","#","$","%","^","&","*","(",")","-","_","=","+"],
 }
-
-// var password = ""
+var inclLower = document.getElementById("inclLower");
+var inclUpper = document.getElementById("inclUpper");
+var inclNumbers = document.getElementById("inclNumbers");
+var inclSymbols = document.getElementById("inclSymbols");
+var passwordLength = document.getElementById("passwordLength");
 // var generateBtn = document.querySelector("#generate");
+// var passwordText = document.getElementById("password");
 
-// Output Password
-// function writePassword() {
-//   var pw = generatePassword();
-//   var passwordText = document.querySelector("#password");
 
-//   passwordText.value = pw;
+// (**Starter Code**) Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword); - ** IS THIS NEEDED???
+document.getElementById("generate").addEventListener("click", obtainParameters);
 
-// }
-
-generateBtn.addEventListener("click", () => {
-  let characters = alpha;
-  // incNumbers.checked ? (characters += numbers) : "";
-  // incSymbols.checked ? (characters += symbols) : "";
-  passwordTxt.value = generatePassword(length.value, characters);
-});
-
-//(**Starter Code**) Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-
-var passwordLength = Number(prompt("Enter a password length between 8 and 128"));
-console.log(passwordLength)
-
-  var lowerPrompt = prompt("Enter Y to include lowercase letters", "Y");
-  if (lowerPrompt === "Y") { 
-    inclLower = Math.floor(Math.random() * lower.length);
-    console.log(lower[inclLower]);
-  } 
-  else {
-    console.log("lowercase letters not included");
+  function obtainParameters() { 
+    var passwordLength = Number(prompt("Enter a password length between 4 and 128"));
+    console.log(passwordLength);
+    // If user pressed cancel, immediately end function
+    if (!passwordLength) {
+      return;
+    }
+    var lowerPrompt = prompt("Enter Y to include lowercase letters", "Y");
+    if (lowerPrompt === "Y") {
+      console.log("lowercase letters included");
+      inclLower = Math.floor(Math.random() * lower.length);
+      console.log(lower[inclLower]);
+    }
+    else {
+      console.log("lowercase letters not included");
+    }
+    var upperPrompt = prompt("Enter Y to include uppercase letters", "Y");
+    if (upperPrompt === "Y") {
+      console.log("uppercase letters included");
+      inclUpper = Math.floor(Math.random() * upper.length);
+      console.log(upper[inclUpper]);
+    }
+    else {
+      console.log("uppercase letters not included");
+    }
+    var numbersPrompt = prompt("Enter Y to include numbers", "Y");
+    if (numbersPrompt === "Y") {
+      console.log("numbers included");
+      inclNumbers = Math.floor(Math.random() * numbers.length);
+      console.log(numbers[inclNumbers]);
+    }
+    else {
+      console.log("numbers not included");
+    }
+    var symbolsPrompt = prompt("Enter Y to include symbols", "Y");
+    if (symbolsPrompt === "Y") {
+      console.log("symbols included");
+      inclSymbols = Math.floor(Math.random() * symbols.length);
+      console.log(symbols[inclSymbols]);
+    }
+    else {
+      console.log("symbols not included");
+    }
   }
 
-  var upperPrompt = prompt("Enter Y to include uppercase letters", "Y");
-  if (upperPrompt === "Y") { 
-    inclUpper = Math.floor(Math.random() * upper.length);
-    console.log(upper[inclUpper]);
-  } 
-  else { 
-    console.log("uppercase letters not included");
-  }
 
-  var numbersPrompt = prompt("Enter Y to include numbers",  "Y");
-  if (numbersPrompt === "Y") {
-    inclNumbers = Math.floor(Math.random() * numbers.length);
-    console.log(numbers[inclNumbers]);
-  }
-  else { 
-    console.log("numbers not included");
-  }
 
-  var symbolsPrompt = prompt("Enter Y to include symbols", "Y");
-  if (symbolsPrompt === "Y") {
-    inclSymbols = Math.floor(Math.random() * symbols.length);
-    console.log(symbols[inclSymbols]);
+// Generate password - NOT WORKING
+  function writePassword(characters, passwordLength) {
+    for (var i = 0; i < passwordLength; i++) {
+    password += Math.floor(Math.random() * characters.length);
+     console.log(writePassword);
+    }
   }
-  else { 
-    console.log("symbols not included");
+writePassword();
+
+  // Output Password
+  function generatePassword() {
+    var pw = writePassword();
+    var passwordText = document.querySelector("#password");
+
+    var password = "";
+    passwordText.value = pw;  
+
+    console.log(password);
+
   }
+generatePassword();
+  // document.getElementById("#password");
 
-  
+  // return ("#password");
 
-// // Is not working with my arrays yet
-// function generatePassword() {
-//   for (var i = 0; i <= passwordLength.length; i++) {
-//   var randomNum = Math.floor(Math.random * characters.length);
-//     password += characters.substring(randomNum, randomNum +1);
-//   console.log(password);
-//   }
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
